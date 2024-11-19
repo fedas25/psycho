c438c9227d6d57f87ab67fc279deff2b67bafb5c
Fedor Kuzin <uasily.lol@yandex.ru>
2024-11-20 00:46:31 +0300

    test fix

--- src/pages/components/Prices/index.jsx
+++ src/pages/components/Prices/index.jsx
@@ -1,7 +1,7 @@
 import style from "./style.module.css"
 import cn from "classnames";
 import Button from "../ui/Button/Button";
-import { linkSession } from "../ui/Link/link";
+import { linkSession } from "../ui/Link";
 
 const Index = () => {
     return (
